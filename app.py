777d2df90d5ffb9e4f00eb8408db6248e3c4dbb0
Dv04 <62090715+Dv04@users.noreply.github.com>
2023-12-18 11:07:31 +0530

    Refactor app.py and model.py, add OCR functionality and display recognized text in index.html

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask_bootstrap import Bootstrap
 from flask import Flask, render_template
 from werkzeug.utils import secure_filename
 from model import ocr_core
+
 app = Flask(__name__)
 
 # Bootstrap
@@ -12,24 +13,27 @@ Bootstrap(app)
 
 # CSRF protection]
 # app.config['SECRET_KEY'] = secrets.token_urlsafe(32)
-app.config['SECRET_KEY'] = 'SECRET-TUNNEL'
-app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config["SECRET_KEY"] = "SECRET-TUNNEL"
+app.config["UPLOAD_FOLDER"] = "uploads"
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def home_page():
     form = UploadPhoto()
     if form.validate_on_submit():
         photo = form.photo.data
         filename = secure_filename(photo.filename)
-        photo_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        
+        photo_path = os.path.join(app.config["UPLOAD_FOLDER"], filename)
+
         print(photo_path)
         photo.save(photo_path)
-        text= ocr_core(photo_path)
+        text = ocr_core(photo_path)
         print(text)
-        return render_template('index.html', form=form, filename=filename, photo_path=photo_path)
-    return render_template('index.html', form=form)
+        return render_template(
+            "index.html", form=form, filename=filename, photo_path=photo_path, text=text
+        )
+    return render_template("index.html", form=form)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
--- model.py
+++ model.py
@@ -1,9 +1,9 @@
 import pytesseract
 from PIL import Image
 
-pytesseract.pytesseract.tesseract_cmd = r'Tesseract-OCR/tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = "/opt/homebrew/bin/tesseract"
+
 
 def ocr_core(filename):
     text = pytesseract.image_to_string(Image.open(filename))
     return text
-
